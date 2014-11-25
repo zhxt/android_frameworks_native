62a4f4a49dfa43e6b201af8e0bbfb84f646e585c
David Greaves <david.greaves@jollamobile.com>
2014-11-25 14:53:30 +0100

    Use libdsyscalls
    Signed-off-by: David Greaves <david.greaves@jollamobile.com>

--- opengl/libs/Android.mk
+++ opengl/libs/Android.mk
@@ -32,7 +32,7 @@ LOCAL_SRC_FILES:= 	       \
 	EGL/Loader.cpp 	       \
 #
 
-LOCAL_SHARED_LIBRARIES += libcutils libutils liblog libGLES_trace
+LOCAL_SHARED_LIBRARIES += libcutils libutils liblog libGLES_trace libdsyscalls
 LOCAL_MODULE:= libEGL
 LOCAL_LDFLAGS += -Wl,--exclude-libs=ALL
 LOCAL_SHARED_LIBRARIES += libdl
@@ -84,7 +84,7 @@ LOCAL_SRC_FILES:= 		\
 	GLES_CM/gl.cpp.arm 	\
 #
 
-LOCAL_SHARED_LIBRARIES += libcutils liblog libEGL
+LOCAL_SHARED_LIBRARIES += libcutils liblog libEGL libdsyscalls
 LOCAL_MODULE:= libGLESv1_CM
 
 LOCAL_SHARED_LIBRARIES += libdl
@@ -108,7 +108,7 @@ LOCAL_SRC_FILES:= 		\
 	GLES2/gl2.cpp.arm 	\
 #
 
-LOCAL_SHARED_LIBRARIES += libcutils libutils liblog libEGL
+LOCAL_SHARED_LIBRARIES += libcutils libutils liblog libEGL libdsyscalls
 LOCAL_MODULE:= libGLESv2
 
 LOCAL_SHARED_LIBRARIES += libdl
